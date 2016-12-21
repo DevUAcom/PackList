808b9599e48e40da06ff1e7256d3d3f4b84c6e8f
Sergiy Oliynyk <olen33@gmail.com>
2016-12-21 16:03:31 +0200

    Table design

--- src/PackList/wwwroot/app/items/addItem.component.controller.spec.js
+++ src/PackList/wwwroot/app/items/addItem.component.controller.spec.js
@@ -1,5 +1,5 @@
-﻿describe('AddItemController' , function () {
-	var AddItemController;
+﻿describe('AddItemComponentController' , function () {
+	var AddItemComponentController;
 
 	beforeEach(function () {
 		bard.appModule('app');
@@ -8,21 +8,21 @@
 
 	it('should call onAdd binding when adding new item', function () {
 		var onAddSpy = jasmine.createSpy('onAdd');
-		AddItemController = $componentController('plAddItem', null, { onAdd: onAddSpy });
+		AddItemComponentController = $componentController('plAddItem', null, { onAdd: onAddSpy });
 
 		var newItem = 'new item';
-		AddItemController.addItem(newItem);
+		AddItemComponentController.addItem(newItem);
 
 		expect(onAddSpy).toHaveBeenCalledWith({item: {name: newItem} });
 	});
 
 	it('should clear model when call addItem', function () {
 		var onAddSpy = jasmine.createSpy('onAdd');
-		AddItemController = $componentController('plAddItem', null, { onAdd: onAddSpy });
+		AddItemComponentController = $componentController('plAddItem', null, { onAdd: onAddSpy });
 
-		AddItemController.name = 'new item';
-		AddItemController.addItem();
+		AddItemComponentController.name = 'new item';
+		AddItemComponentController.addItem();
 
-		expect(AddItemController.name).toEqual('');
+		expect(AddItemComponentController.name).toEqual('');
 	});
 });
