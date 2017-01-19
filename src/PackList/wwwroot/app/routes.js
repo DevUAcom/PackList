26c8b8c018e24cc997223933942050115c68416e
Sergiy Oliynyk <olen33@gmail.com>
2017-01-19 19:01:26 +0200

    Changed layout

--- src/PackList/wwwroot/app/routes.js
+++ src/PackList/wwwroot/app/routes.js
@@ -9,34 +9,34 @@
 		$urlRouterProvider.otherwise('/');
 
 		$stateProvider
-		  .state('home', { url: '/', template: '<a ui-sref="luggage">luggage</a>' })
-		  // Luggage
-		.state('luggage',
-		  {
-		  	url: '/luggage',
-		  	templateUrl: 'app/luggage/luggage.template.html',
-		  	controller: 'LuggageController as luggageCtrl',
-		  	resolve: {
-		  		luggageList: [
-				  'LuggageService', function (LuggageService) {
-				  	return LuggageService.getLuggage();
-				  }
-		  		]
-		  	}
-		  })
-		// Items
-		.state('items', {
-			url: '/items',
-			templateUrl: 'app/items/items.template.html',
-			controller: 'ItemsController as itemsCtrl',
-			resolve: {
-				itemsList: [
-				  'ItemsService', function (ItemsService) {
-				  	return ItemsService.getItems();
-				  }
-				]
-			}
-		});
+			.state('home', { url: '/', template: '<h1>Dashboard</h1>' })
+			// Luggage
+			.state('luggage',
+			  {
+		  		url: '/luggage',
+		  		templateUrl: 'app/luggage/luggage.template.html',
+		  		controller: 'LuggageController as luggageCtrl',
+		  		resolve: {
+		  			luggageList: [
+					  'LuggageService', function (LuggageService) {
+				  		return LuggageService.getLuggage();
+					  }
+		  			]
+		  		}
+			  })
+			// Items
+			.state('items', {
+				url: '/items',
+				templateUrl: 'app/items/items.template.html',
+				controller: 'ItemsController as itemsCtrl',
+				resolve: {
+					itemsList: [
+					  'ItemsService', function (ItemsService) {
+				  		return ItemsService.getItems();
+					  }
+					]
+				}
+			});
 	}
 
 })();
